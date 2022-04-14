b15fa9488eb3be87a0545ee3ae7911a323a9f479
GuntipallySatwika <98134060+GuntipallySatwika@users.noreply.github.com>
2022-04-14 18:02:32 -0500

    Update cars.js

--- controllers/cars.js
+++ controllers/cars.js
@@ -12,10 +12,17 @@ exports.cars_list = async function(req, res) {
     }   
 };
  
-// for a specific Cars. 
-exports.cars_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: car detail: ' + req.params.id); 
-}; 
+// for a specific Costume. 
+exports.cars_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await cars.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+};  
  
 // Handle Cars create on POST. 
  
@@ -45,9 +52,25 @@ exports.cars_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: Car delete DELETE ' + req.params.id); 
 }; 
  
-// Handle Car update form on PUT. 
-exports.cars_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Car update PUT' + req.params.id); 
+// Handle Costume update form on PUT. 
+exports.cars_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await cars.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.car_company)  
+               toUpdate.car_company = req.body.cars_type; 
+        if(req.body.cost) toUpdate.color = req.body.color; 
+        if(req.body.size) toUpdate.price = req.body.price; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
 }; 
 // VIEWS 
 // Handle a show all view 
