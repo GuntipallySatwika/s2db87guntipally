62c611483ad941f0ea8b3c06e26e6d37079d9e39
GuntipallySatwika <98134060+GuntipallySatwika@users.noreply.github.com>
2022-04-25 13:06:49 -0500

    commit

--- models/cars.js
+++ models/cars.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose")
 const carsSchema = mongoose.Schema({ 
  car_company: String, 
  color: String, 
- price: Number 
+ price: {type:Number,
+min:1000,
+max:2000000000000000} 
 }) 
  
 module.exports = mongoose.model("cars", carsSchema) 
--- routes/cars.js
+++ routes/cars.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const cars_controllers= require('../controllers/cars');
-
+// A little function to check if we have an authorized user and continue on or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 /* GET home page. */
   router.get('/', cars_controllers.cars_view_all_Page );
   /* GET detail cars page */ 
@@ -9,7 +17,7 @@ router.get('/detail', cars_controllers.cars_view_one_Page);
 /* GET create cars page */ 
 router.get('/create', cars_controllers.cars_create_Page); 
 /* GET create update page */ 
-router.get('/update', cars_controllers.cars_update_Page); 
+router.get('/update',secured, cars_controllers.cars_update_Page); 
 /* GET delete cars page */ 
 router.get('/delete', cars_controllers.cars_delete_Page); 
 module.exports = router;
